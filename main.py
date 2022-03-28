d39c307e721802ac103efb8153c29a8ba5cfdbc8
celebrater <tkgm45nbchk@gmail.com>
2022-03-29 01:56:46 +0900

    rewrote the filename "favicon"

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ def convert_pic(filename):
     img = Image.open(filename)
     # img.save('output/hoge.ico')
     icon_sizes = [(16, 16), (32, 32), (48, 48), (64, 64)]
-    img.save('output/hoge.ico', sizes=icon_sizes)
+    img.save('output/favicon.ico', sizes=icon_sizes)
 
 if __name__ == '__main__':
     convert_pic('input/hoge.png')
